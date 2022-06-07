6b4b31e9fba7a31781918c7ffa700a48166a1bbe
Mohammed Ali P S <psmohammedali4@gmail.com>
2022-06-07 17:33:02 +0530

    arithmetic and range files

---
+++ 7-6-2022/arithmetic.py
@@ -0,0 +1,14 @@
+# SI from pnr
+p = 1000
+n = 2
+r = 8
+si = (p*n*r)/100
+print(si)
+si1 = (p*n*r)//100
+print(si1)
+
+
+# farenhit to celsius
+f = 100
+c = ((f-32) * 5)/9
+print(c)
---
+++ 7-6-2022/range.py
@@ -0,0 +1,6 @@
+a = 10
+print(a)
+a = 10000000000000000000000000000000000
+print(a)
+
+# There is no restriction in the range of data limit to be stored
