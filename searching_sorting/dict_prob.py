8870a1a080091e1f9b9697dea0d11c91d5fc0e31
Mohammed Ali P S <psmohammedali4@gmail.com>
2022-08-19 00:39:47 +0530

    list part-1

--- searching_sorting/dict_prob.py
+++ searching_sorting/dict_prob.py
@@ -6,3 +6,8 @@
 # #     "name": 2
 # # }
 # # len([1 for i in items if i[rule_index[ruleKey]] == ruleValue])
+s = "hello world"
+my_list = s.split()
+print(my_list)
+capital_letter = chr(ord("A") +(ord('i') - ord('a')))
+print(capital_letter)
